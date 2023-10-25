6f827f47ea7cb685093a7b4be6ef54fe1e6ec34e
Mamun Joy <mamunjoy4291@gmail.com>
2023-10-25 23:31:57 +0600

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require ('./app')
+const connectDatabase = require('./config/database')
 
 
 const dotenv = require('dotenv');
@@ -6,6 +7,11 @@ const dotenv = require('dotenv');
 // setting up config file
 dotenv.config({path: 'backend/config/config.env' })
 
+
+
+//connecting to database
+connectDatabase();
+
 app.listen (process.env.PORT, () => {
-    console.log(`server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} node.`)
+    console.log(`server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} node.`)
 })
