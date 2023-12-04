472ae560c058439e8f441216ea04828c6a2a0ba4
Mamun Joy <mamunjoy4291@gmail.com>
2023-12-04 20:42:50 +0600

    Products add part

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,15 @@
+const product = require('../models/product')
 
+//create new product   => /api/v1/product/new
+exports.newproduct = asyc (req,res , next) => {
+
+    const product = await product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
 exports.getProducts = (reg, res, next) => {
     res.status(200).json({
         success: true,
