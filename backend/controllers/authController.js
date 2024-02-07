8f13823a417a5030fad9d403005ff0b075cf0ac5
Mamun Joy <mamunjoy4291@gmail.com>
2024-02-07 21:27:31 +0600

    Complete next part

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,3 +53,18 @@ if (!email || !password){
 
     sendToken(user, 200, res)
 })
+
+
+// Logout user    => /api/v1/logout
+exports.logout = catchAsyncErrors(async (req,res,next) => {
+    res.cookie('token', null, { 
+        expires: new Date(Date.now()),
+        httpOnly: true
+        
+    })
+
+    res.status(200).json({
+        success:true,
+        message:'Logged out successfully'
+    })
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -14,14 +14,14 @@ const {
 const { isAuthenticatedUser } = require("../middlewares/auth");
 
 
-router.route('/products').get(isAuthenticatedUser, getProducts);
+router.route('/products').get(getProducts);
 router.route('/products/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, newProduct);
 
 router.route('/admin/products/:id')
-        .put(updateProduct)
-        .delete(deleteProduct);
+        .put(isAuthenticatedUser, updateProduct)
+        .delete(isAuthenticatedUser, deleteProduct);
 
 module.exports = router;
 
